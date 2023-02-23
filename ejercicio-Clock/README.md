bbfabc3adda911761410ced3027b7787ca08d952
emanuelLeiva <leiva92emanuel@gmail.com>
2023-02-23 17:41:03 -0300

    ejercicios de React

---
+++ ejercicio-Clock/README.md
@@ -0,0 +1,1 @@
+Partiendo del componente de clase que contempla varios métodos de ciclo de vida, convertidlo en un componente funcional que realice exactamente lo mismo:
---
+++ ejercicio-Clock/src/App.js
@@ -0,0 +1,10 @@
+import ClockClase from "./component/clockClase";
+import ClockFuncional from "./component/clockClase";
+
+function App() {
+  return (
+<ClockFuncional></ClockFuncional>
+)
+}
+
+export default App;
